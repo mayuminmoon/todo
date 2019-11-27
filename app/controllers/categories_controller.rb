5861434c50cd390c5e09c72c73eb4b0b6c6b5409
Mayumi Suzuki <mayumi@thinkific.com>
2019-11-27 11:43:58 -0800

    add create and show category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,48 @@
 class CategoriesController < ApplicationController
+  #before_action :set_category, only: [:show, :edit, :update, :destroy]
+
   def index
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    #render plain: params[:category].inspect
+    @category = Category.new(category_params)
+
+    @category.save
+    redirect_to @category
+
+    # @categories = Category.new(params.require(:category).permit(:title))
+    # @categories.category_id = params[:category_id] 
+
+    # respond_to do |format|
+    #   if @categories.save
+    #     format.html { redirect_to @categories, notice: 'Category  was successfully created.' }
+    #     format.json { render :show, status: :created, location: @categories }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @categories.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+end
+
+ private
+#   # Use callbacks to share common setup or constraints between actions.
+#   def set_category
+#     @category = Category.find(params[:category_id])
+#   end
+def category_params
+  params.require(:category).permit(:title)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :todo_lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "todo_lists#index"
-  get 'category', to: 'categories#index'
+  get 'categories', to: 'categories#index'
+  get 'categories/new', to: 'categories#new', as: 'new_category'
+  resources :categories
+  #get 'categories/:id', to: 'categories#show', as: 'show_category'
+  #get 'categories/:id', to: 'categories#edit', as: 'edit_category'
 end
